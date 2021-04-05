72fe1dbeeddd61e0b1f9ac06585b5788db32462f
Kacper Mura <kacper.mura@web.de>
2021-04-05 15:57:21 +0200

    ✨New location for artworks

--- src/services/artwork.service.ts
+++ src/services/artwork.service.ts
@@ -7,52 +7,55 @@ import { LogService } from "./log.service";
 export namespace ArtworkService {
   export function getErrorArtworks() {
     return {
-      1000: "https://cdn.atomicradio.eu/fallback/1000-060902000.jpg",
-      500: "https://cdn.atomicradio.eu/fallback/0500-060902000.jpg",
-      250: "https://cdn.atomicradio.eu/fallback/0250-060902000.jpg",
-      100: "https://cdn.atomicradio.eu/fallback/0100-060902000.jpg"
+      1000: "https://cdn.atomicradio.eu/artworks/fallback/1000.jpg",
+      500: "https://cdn.atomicradio.eu/artworks/fallback/0500.jpg",
+      250: "https://cdn.atomicradio.eu/artworks/fallback/0250.jpg",
+      100: "https://cdn.atomicradio.eu/artworks/fallback/0100.jpg"
     };
   }
 
   export function getStreamerArtworks(streamerName: string) {
     const name = String(streamerName).toLowerCase().replace(/ /g, "");
     return {
-      1000: `https://cdn.atomicradio.eu/streamer/1000-${name}.jpg`,
-      500: `https://cdn.atomicradio.eu/streamer/0500-${name}.jpg`,
-      250: `https://cdn.atomicradio.eu/streamer/0250-${name}.jpg`,
-      100: `https://cdn.atomicradio.eu/streamer/0100-${name}.jpg`
+      1000: `https://cdn.atomicradio.eu/streamer/${name}/1000.jpg`,
+      500: `https://cdn.atomicradio.eu/streamer/${name}/0500.jpg`,
+      250: `https://cdn.atomicradio.eu/streamer/${name}/0250.jpg`,
+      100: `https://cdn.atomicradio.eu/streamer/${name}/0100.jpg`
     };
   }
 
   export function getArtworks(id: string, artwork: string) {
-    if (artwork !== "https://cdn.atomicradio.eu/fallback/1000-060902000.jpg") {
-      if (!fs.existsSync(`./assets/artworks/1000-${id}.jpg`)) {
+    if (artwork !== "https://cdn.atomicradio.eu/artworks/fallback/1000.jpg") {
+      if (!fs.existsSync(`./assets/artworks/${id}`)) {
+        fs.mkdirSync(`./assets/artworks/${id}`);
+
         axios
           .get(artwork, { responseType: "stream" })
           .then((response) => {
-            const writer = fs.createWriteStream(`./assets/artworks/1000-${id}.jpg`);
+            const writer = fs.createWriteStream(`./assets/artworks/${id}/1000.jpg`);
             response.data.pipe(writer);
+
             writer.on("finish", () => {
-              sharp(`./assets/artworks/1000-${id}.jpg`)
+              sharp(`./assets/artworks/${id}/1000.jpg`)
                 .resize(500, 500)
                 .jpeg({ quality: 100, chromaSubsampling: "4:4:4", progressive: true })
-                .toFile(`./assets/artworks/0500-${id}.jpg`)
+                .toFile(`./assets/artworks/${id}/0500.jpg`)
                 .catch((error) => {
                   LogService.logError("The size of one artwork could not be resized to 500x500. (" + id + ")");
                   console.log(error);
                 });
-              sharp(`./assets/artworks/1000-${id}.jpg`)
+              sharp(`./assets/artworks/${id}/1000.jpg`)
                 .resize(250, 250)
                 .jpeg({ quality: 100, chromaSubsampling: "4:4:4", progressive: true })
-                .toFile(`./assets/artworks/0250-${id}.jpg`)
+                .toFile(`./assets/artworks/${id}/0250.jpg`)
                 .catch((error) => {
                   LogService.logError("The size of one artwork could not be resized to 250x250. (" + id + ")");
                   console.log(error);
                 });
-              sharp(`./assets/artworks/1000-${id}.jpg`)
+              sharp(`./assets/artworks/${id}/1000.jpg`)
                 .resize(100, 100)
                 .jpeg({ quality: 100, chromaSubsampling: "4:4:4", progressive: true })
-                .toFile(`./assets/artworks/0100-${id}.jpg`)
+                .toFile(`./assets/artworks/${id}/0100.jpg`)
                 .catch((error) => {
                   LogService.logError("The size of one artwork could not be resized to 100x100. (" + id + ")");
                   console.log(error);
@@ -67,11 +70,12 @@ export namespace ArtworkService {
             console.log(error);
           });
       }
+
       return {
-        1000: `https://cdn.atomicradio.eu/artworks/1000-${id}.jpg`,
-        500: `https://cdn.atomicradio.eu/artworks/0500-${id}.jpg`,
-        250: `https://cdn.atomicradio.eu/artworks/0250-${id}.jpg`,
-        100: `https://cdn.atomicradio.eu/artworks/0100-${id}.jpg`
+        1000: `https://cdn.atomicradio.eu/artworks/${id}/1000.jpg`,
+        500: `https://cdn.atomicradio.eu/artworks/${id}/0500.jpg`,
+        250: `https://cdn.atomicradio.eu/artworks/${id}/0250.jpg`,
+        100: `https://cdn.atomicradio.eu/artworks/${id}/0100.jpg`
       };
     } else {
       return getErrorArtworks();
