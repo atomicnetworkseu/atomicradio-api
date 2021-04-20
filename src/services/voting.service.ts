472a4cd81a5b87750d8570727541029680fbe3d9
Kacper Mura <kacper.mura@web.de>
2021-04-20 14:37:57 +0200

    Set cache duration & ending timestamp

--- src/services/voting.service.ts
+++ src/services/voting.service.ts
@@ -21,12 +21,14 @@ export namespace VotingService {
                     artworks: ArtworkService.getArtworks(media.media.id, "http://" + process.env.AZURACAST_API + media.media.art)
                 });
             }
+            const endingDate = new Date();
+            endingDate.setDate(endingDate.getDate() + (7 + 5 - endingDate.getDay() - 1) % 7 +1);
             const voting: VotingModel = {
                 items: result,
-                created_at: Math.round(new Date().getTime()/1000),
-                ending_at: 0
+                created_at: new Date().getTime(),
+                ending_at: new Date(endingDate.getFullYear(), endingDate.getMonth(), endingDate.getDate(), 19).getTime()
             };
-            CacheService.set("voting", voting);
+            CacheService.set("voting", voting, new Date(endingDate.getFullYear(), endingDate.getMonth(), endingDate.getDate(), 18).getTime()-new Date().getTime());
         }).catch(() => {
             LogService.logError("Error while reading azuracast media list.");
         });
