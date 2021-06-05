f2caf1373bc3159fbdd82b1f5340fe4b18300c2e
Kacper Mura <kacper.mura@web.de>
2021-06-05 11:36:03 +0200

    Update radioboss.service.ts

--- src/services/radioboss.service.ts
+++ src/services/radioboss.service.ts
@@ -7,7 +7,7 @@ export namespace RadioBossService {
 
     export function getPlayBackInfo(station: string): Promise<PlayBackInfo> {
         return new Promise((resolve, reject) => {
-            axios.get(`http://${station}.music.ufo.atnw.eu:3000/?pass=bTGPvNUb4pLYtQxKYWtb&action=playbackinfo`).then((response) => {
+            axios.get(`http://${station}.music.ufo.atnw.eu:3000/?pass=${process.env.RADIOBOSS_PASSWORD}&action=playbackinfo`).then((response) => {
                 const data = parser.toJson(response.data);
                 const playBackInfo = JSON.parse(data) as PlayBackInfo;
                 resolve(playBackInfo);
@@ -20,7 +20,7 @@ export namespace RadioBossService {
 
     export function getLastPlayed(station: string): Promise<LastPlayed> {
         return new Promise((resolve, reject) => {
-            axios.get(`http://${station}.music.ufo.atnw.eu:3000/?pass=bTGPvNUb4pLYtQxKYWtb&action=getlastplayed`).then((response) => {
+            axios.get(`http://${station}.music.ufo.atnw.eu:3000/?pass=${process.env.RADIOBOSS_PASSWORD}&action=getlastplayed`).then((response) => {
                 const data = parser.toJson(response.data);
                 const lastPlayed = JSON.parse(data) as LastPlayed;
                 resolve(lastPlayed);
@@ -33,7 +33,7 @@ export namespace RadioBossService {
 
     export function getPlaylist(station: string): Promise<Playlist> {
         return new Promise((resolve, reject) => {
-            axios.get(`http://${station}.music.ufo.atnw.eu:3000/?pass=bTGPvNUb4pLYtQxKYWtb&action=getplaylist2`).then((response) => {
+            axios.get(`http://${station}.music.ufo.atnw.eu:3000/?pass=${process.env.RADIOBOSS_PASSWORD}&action=getplaylist2`).then((response) => {
                 const data = parser.toJson(response.data);
                 const playlist = JSON.parse(data) as Playlist;
                 resolve(playlist);
@@ -46,7 +46,7 @@ export namespace RadioBossService {
 
     export function getCurrentArtwork(station: string): Promise<AxiosResponse<any>> {
         return new Promise((resolve, reject) => {
-            axios.get(`http://${station}.music.ufo.atnw.eu:3000/?pass=bTGPvNUb4pLYtQxKYWtb&action=trackartwork`, { responseType: "stream" }).then((response) => {
+            axios.get(`http://${station}.music.ufo.atnw.eu:3000/?pass=${process.env.RADIOBOSS_PASSWORD}&action=trackartwork`, { responseType: "stream" }).then((response) => {
                 resolve(response);
             }).catch((err) => {
                 LogService.logError("Error while reading radioboss current artwork informations. (" + station + ")");
@@ -57,7 +57,7 @@ export namespace RadioBossService {
 
     export function requestSong(filename: string): Promise<any> {
         return new Promise((resolve, reject) => {
-            axios.get("http://one.music.ufo.atnw.eu:3000/?pass=bTGPvNUb4pLYtQxKYWtb&action=songrequest&filename=" + encodeURIComponent(filename) + "&message=" + encodeURIComponent("REQUESTED SONG BY API")).then((response) => {
+            axios.get(`http://one.music.ufo.atnw.eu:3000/?pass=${process.env.RADIOBOSS_PASSWORD}&action=songrequest&filename=` + encodeURIComponent(filename) + "&message=" + encodeURIComponent("REQUESTED SONG BY API")).then((response) => {
                 resolve(response);
             }).catch((err) => {
                 LogService.logError("Error while sending the song suggestion.");
